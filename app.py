3191c31b1da5982a179837bb3d4b30ee25a52fd9
magfur2309 <magfurgezo@gmail.com>
2025-02-20 13:15:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -114,14 +114,8 @@ def login_page():
 
 def main_app():
     st.title("Convert Faktur Pajak PDF To Excel")
-
-    # Tombol Logout
-    if st.sidebar.button("Logout 🚪"):
-        st.session_state["logged_in"] = False
-        st.experimental_rerun()  # Refresh halaman untuk kembali ke login
-
     uploaded_files = st.file_uploader("Upload Faktur Pajak (PDF, bisa lebih dari satu)", type=["pdf"], accept_multiple_files=True)
-
+    
     if uploaded_files:
         all_data = []
         for uploaded_file in uploaded_files:
@@ -129,23 +123,28 @@ def main_app():
             detected_item_count = count_items_in_pdf(uploaded_file)
             extracted_data = extract_data_from_pdf(uploaded_file, tanggal_faktur, detected_item_count)
             extracted_item_count = len(extracted_data)
-
+            
             if detected_item_count != extracted_item_count and detected_item_count != 0:
                 st.warning(f"Jumlah item tidak cocok untuk {uploaded_file.name}: Ditemukan {detected_item_count}, diekstrak {extracted_item_count}")
-
+            
             if extracted_data:
                 all_data.extend(extracted_data)
-
+        
         if all_data:
             df = pd.DataFrame(all_data, columns=["No FP", "Nama Penjual", "Nama Pembeli", "Tanggal Faktur", "Nama Barang", "Qty", "Satuan", "Harga", "Potongan Harga", "Total", "DPP", "PPN"])
             df.index = df.index + 1  
             st.write("### Pratinjau Data yang Diekstrak")
             st.dataframe(df)
-
             output = io.BytesIO()
             with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
                 df.to_excel(writer, index=True, sheet_name='Faktur Pajak')
             output.seek(0)
-            
             st.download_button(label="\U0001F4E5 Unduh Excel", data=output, file_name="Faktur_Pajak.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 
+if "logged_in" not in st.session_state:
+    st.session_state["logged_in"] = False
+
+if not st.session_state["logged_in"]:
+    login_page()
+else:
+    main_app()
