5a748b4a9a703fff782fd1cb389714bc14f44edc
magfur2309 <magfurgezo@gmail.com>
2025-05-22 16:27:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -92,6 +92,8 @@ def login_page():
     users = {
         "user1": hashlib.sha256("ijfugroup1".encode()).hexdigest(),
         "user2": hashlib.sha256("ijfugroup2".encode()).hexdigest()
+        "user3": hashlib.sha256("ijfugroup3".encode()).hexdigest()
+        "user4": hashlib.sha256("ijfugroup4".encode()).hexdigest()
         
     }
     
